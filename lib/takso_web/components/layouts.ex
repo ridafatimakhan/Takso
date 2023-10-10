0a1aa3c5b6287d3ffe313a5a64c72d85cb802977
Rida Fatima <92318911+ridafatimakhan@users.noreply.github.com>
2023-10-10 19:10:32 +0300

    first setup commit

---
+++ lib/takso_web/components/layouts.ex
@@ -0,0 +1,5 @@
+defmodule TaksoWeb.Layouts do
+  use TaksoWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/takso_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule TaksoWeb.PageHTML do
+  use TaksoWeb, :html
+
+  embed_templates "page_html/*"
+end
