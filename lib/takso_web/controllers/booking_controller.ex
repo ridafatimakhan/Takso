761a6ab1b9a1b393ae270dfb3698e06d082ea36c
Rida Fatima <92318911+ridafatimakhan@users.noreply.github.com>
2023-10-23 22:28:06 +0300

    [Part 3: Ecto queries ]

--- lib/takso_web/controllers/booking_controller.ex
+++ lib/takso_web/controllers/booking_controller.ex
@@ -1,4 +1,6 @@
 defmodule TaksoWeb.BookingController do
+  import Ecto.Query, only: [from: 2]
+  alias Takso.{Repo,Sales.Taxi}
   use TaksoWeb, :controller
 
   def new(conn, _params) do
@@ -10,10 +12,35 @@ defmodule TaksoWeb.BookingController do
   end
 
   def create(conn, _params) do
-    conn
+    query = from(t in Taxi, where: t.status == "available")
+    available_taxis = Repo.all(query)
+    if Enum.empty?(available_taxis) do
+      conn
+      |> put_flash(:info, "At present, there is no taxi available!")
+      |> redirect(to: ~p"/bookings")
+    else
+      conn
       |> put_flash(:info, "Your taxi will arrive in 15 minutes.")
       |> redirect(to: ~p"/bookings")
     end
-
-
+  end
 end
+  # def create(conn, _params) do
+  #   query = from(t in Taxi, where: t.status == "available")
+  #   available_taxis = Repo.all(query)
+  #   available_taxis_count = length(available_taxis)
+  #   if available_taxis_count = 0 do
+  #     flash_message = "At present, there are no taxis available!"
+  #     conn
+  #       |> put_flash(:info, flash_message)
+  #       |> redirect(to: Routes.booking_path(conn, :index))
+
+  #   else
+  #     flash_message = "There are #{available_taxis_count} available taxis. Your taxi will arrive in 15 minutes."
+  #     conn
+  #       |> put_flash(:info, flash_message)
+  #       |> redirect(to: Routes.booking_path(conn, :index))
+
+
+  #   end
+  # end
